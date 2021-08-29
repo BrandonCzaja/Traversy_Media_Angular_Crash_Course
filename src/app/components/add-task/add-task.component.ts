a51b36b835996afe0daafedeef094581d15a9c2e
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-29 17:55:53 -0500

    Can now add tasks

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task'; // Task Interface. Required for @Output EventEmitter
 
 @Component({
   selector: 'app-add-task',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
   // When working with forms each field needs a property
   text: string;
   day: string;
@@ -14,4 +17,25 @@ export class AddTaskComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    // Emit with newTask
+    this.onAddTask.emit(newTask);
+
+    // These reset the form to blank
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }

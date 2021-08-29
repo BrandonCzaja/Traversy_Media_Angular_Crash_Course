2f7913bfcb39f8afacea0ba872546b3a8a2510ab
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-29 14:29:51 -0500

    Created a task-item component that displays the tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { TASKS } from '../../mock-tasks';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  // tasks is type Task, which is an array. It is set to the mock data
+  tasks: Task[] = TASKS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

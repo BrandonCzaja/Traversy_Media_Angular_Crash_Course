70ba4eb1f8eadc029ab9ea0f524a130ac020f10f
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-29 11:50:39 -0500

    Added some notes about Angular structure and the styling from the repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; // Every Angular component requires this
 
+// Component Declaration
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // Selector is the tag (html tag) that you will use to embed your component. I think like parent component
+  templateUrl: './app.component.html', // HTML File that you are using
+  styleUrls: ['./app.component.css'], // Styles, can have more than one
 })
+
+// Properties and methods go in this class. Kind of like React class-based components
 export class AppComponent {
-  title = 'angular-crash-course';
+  // You should add data type to your properties
+  title: string = 'Task Tracker';
 }

0022a4dfd2926d952207444bd0db0ebd87530e72
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-29 17:38:53 -0500

    Imported Forms Module for two-way data binding with add-task component in app.module.ts

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
+  // When working with forms each field needs a property
+  text: string;
+  day: string;
+  reminder: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

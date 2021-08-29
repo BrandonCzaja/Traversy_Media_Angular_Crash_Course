8f0f1f5f30174be4f3ea5b1b6aba1b4b6422f8d4
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-29 18:12:28 -0500

    Add Task button toggles, but the functionality isn't setup yet

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   // You should add data type to your properties
   title: string = 'Task Tracker';
+  showAddTask: boolean = true;
+  subscription: Subscription;
 
-  constructor() {}
+  // In order to use a Service it has to be added to the constructor
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   // Lifecycle method - Similar to useEffect()
   ngOnInit(): void {}
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask(); // Calls toggleAddTask() from ui.service.ts
   }
 }
 
